21133ef51e0c996bfe20870766cba33e9a62efc7
jasr <jasoria@uci.cu>
2015-12-14 22:50:26 -0500

    finished support for EURO on snapshot.

--- src/main/resources/sql/V0.6.2__modified_key_snapshot.sql
+++ src/main/resources/sql/V0.6.2__modified_key_snapshot.sql
@@ -2,3 +2,6 @@ ALTER TABLE `currentcy`.`snapshot`
 CHANGE COLUMN `currency` `currency` VARCHAR(4) NOT NULL ,
 DROP PRIMARY KEY,
 ADD PRIMARY KEY (`code`, `currency`);
+
+ALTER TABLE `currentcy`.`snapshot` 
+DROP INDEX `code_UNIQUE` ;
