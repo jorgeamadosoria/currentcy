0bace370e471303e9b423455366e787dfd469006
jorge.amado <darksoul.uci@gmail.com>
2016-03-11 10:09:14 -0300

    removed several exchanges that do not update their websites

--- src/main/java/org/jasr/currentcy/samplers/MainTest.java
+++ src/main/java/org/jasr/currentcy/samplers/MainTest.java
@@ -9,7 +9,7 @@ public class MainTest {
     public static void main(String[] args) {
         SamplerConfig var = new SamplerConfig();
         SamplerBase[] samplers = new SamplerBase[] { 
-                var.AVE()};
+                var.AeroM()};
 
         for (SamplerBase sampler : samplers) {
             sampler.setTimeout(0);
